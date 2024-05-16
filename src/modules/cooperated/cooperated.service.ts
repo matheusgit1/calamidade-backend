8d364b6936e2dfc4787eb61843a9f8718b392f89
kelvinsb <kelvinbr11@gmail.com>
2024-05-16 20:05:54 -0300

    feat: add organization on cooperated create routes

--- src/modules/cooperated/cooperated.service.ts
+++ src/modules/cooperated/cooperated.service.ts
@@ -1,46 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCooperatedDto } from './dto/create-cooperated.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Cooperated } from './entities/cooperated.entity';
-import { DeepPartial, Repository, getManager } from 'typeorm';
-import { IPaginationOptions } from '../../utils/types/pagination-options';
-import { EntityCondition } from '../../utils/types/entity-condition.type';
-import { NullableType } from '../../utils/types/nullable.type';
+import { Injectable, UnprocessableEntityException } from "@nestjs/common";
+import { CreateCooperatedDto } from "./dto/create-cooperated.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Cooperated } from "./entities/cooperated.entity";
+import { DeepPartial, Repository } from "typeorm";
+import { IPaginationOptions } from "../../utils/types/pagination-options";
+import { EntityCondition } from "../../utils/types/entity-condition.type";
+import { NullableType } from "../../utils/types/nullable.type";
+import { OrganizationService } from "../organization/organization.service";
 
 @Injectable()
 export class CooperatedService {
   constructor(
     @InjectRepository(Cooperated)
     private cooperatedRepository: Repository<Cooperated>,
+    private readonly organizationService: OrganizationService,
   ) {}
 
-  create(createCooperatedDto: CreateCooperatedDto) {
-    return this.cooperatedRepository.save(
-      this.cooperatedRepository.create(createCooperatedDto),
-    );
+  async create(createCooperatedDto: CreateCooperatedDto) {
+    if (!createCooperatedDto.document) throw new UnprocessableEntityException("document should not be empty");
+    if (!createCooperatedDto.organization) throw new UnprocessableEntityException("organization should not be empty");
+
+    const normalizedDocument = createCooperatedDto.document.replace(/\D/g, "");
+    const alreadyOneWithDocument = await this.cooperatedRepository.findOne({
+      where: {
+        document: normalizedDocument,
+      },
+    });
+    if (alreadyOneWithDocument) throw new UnprocessableEntityException("document already exists");
+
+    const organization = await this.organizationService.findOne({ id: +createCooperatedDto.organization });
+    if (!organization) throw new UnprocessableEntityException("organization of provided organization is not found");
+
+    return this.cooperatedRepository.save(this.cooperatedRepository.create({ ...createCooperatedDto, document: normalizedDocument, organization }));
   }
 
-  findManyWithPagination(
-    paginationOptions: IPaginationOptions,
-  ): Promise<Cooperated[]> {
+  findManyWithPagination(paginationOptions: IPaginationOptions): Promise<Cooperated[]> {
     return this.cooperatedRepository.find({
       skip: (paginationOptions.page - 1) * paginationOptions.limit,
       take: paginationOptions.limit,
     });
   }
 
-  findOne(
-    fields: EntityCondition<Cooperated>,
-  ): Promise<NullableType<Cooperated>> {
+  findOne(fields: EntityCondition<Cooperated>): Promise<NullableType<Cooperated>> {
     return this.cooperatedRepository.findOne({
       where: fields,
     });
   }
 
-  update(
-    id: Cooperated['id'],
-    payload: DeepPartial<Cooperated>,
-  ): Promise<Cooperated> {
+  update(id: Cooperated["id"], payload: DeepPartial<Cooperated>): Promise<Cooperated> {
     return this.cooperatedRepository.save(
       this.cooperatedRepository.create({
         id,
@@ -49,18 +56,19 @@ export class CooperatedService {
     );
   }
 
-  async softDelete(id: Cooperated['id']): Promise<void> {
+  async softDelete(id: Cooperated["id"]): Promise<void> {
     await this.cooperatedRepository.softDelete(id);
   }
 
   async createBulk(createCooperatedDtos: CreateCooperatedDto[]): Promise<void> {
-    const cooperatedEntities = createCooperatedDtos.map((dto) => {
+    const cooperatedEntities = createCooperatedDtos.map(dto => {
       const cooperated = new Cooperated();
       cooperated.email = dto.email;
       cooperated.firstName = dto.firstName;
       cooperated.lastName = dto.lastName;
       cooperated.phone = dto.phone;
       cooperated.document = dto.document;
+      cooperated.organization = dto.organization;
       return cooperated;
     });
 
