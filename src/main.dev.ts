6005f790ba6f1eb3e7351488ea3281bb0689deb4
Gustavo Valle <gustavo.vallerp@hotmail.com>
2024-05-17 16:51:57 +0200

    :sparkles: feat: increasing payload limit

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -1,5 +1,8 @@
 import { bootstrap } from "./main";
+import { urlencoded, json } from 'express';
 
 void bootstrap().then(async ({ app, config }) => {
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(config.getOrThrow("app.port", { infer: true }));
 });
