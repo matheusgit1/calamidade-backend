f6c25e90db286b6e719fe669f76ca1aeeff57595
Alexander Braga <alexanderbraga46@gmail.com>
2024-05-17 19:13:57 -0300

    remove twitter and facebook

--- src/modules/cooperated/dto/update-cooperated.dto.ts
+++ src/modules/cooperated/dto/update-cooperated.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCooperatedDto } from './create-cooperated.dto';
 
-export class UpdateCooperatedDto extends PartialType(CreateCooperatedDto) {}
+export class UpdateCooperatedDto {}
