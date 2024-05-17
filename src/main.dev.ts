fedaaa6268145687c4022d96ea64d3c7e5eccf08
ayusuke7 <alexhcb.7392@gmail.com>
2024-05-17 16:50:07 -0300

    feat: increasing payload limit again

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -1,8 +1,5 @@
 import { bootstrap } from "./main";
-import { urlencoded, json } from 'express';
 
 void bootstrap().then(async ({ app, config }) => {
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(config.getOrThrow("app.port", { infer: true }));
 });
