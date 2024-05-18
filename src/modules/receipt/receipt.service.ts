ccf7f49b49459621ec37bc0bb42b7628e8d548fc
Gustavo Valle <gustavo.vallerp@hotmail.com>
2024-05-18 22:38:30 +0200

    :bug: fix: adjusting missing imports and rollback code deleted

--- src/modules/receipt/receipt.service.ts
+++ src/modules/receipt/receipt.service.ts
@@ -2,9 +2,12 @@ import { Injectable, BadRequestException } from "@nestjs/common";
 import { CreateReceiptDto } from "./dto/create-receipt.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ReceiptEntity } from "./entities/receipt.entity";
-import { Repository } from "typeorm";
+import { DeepPartial, Repository } from "typeorm";
 import { RequestEntity } from "src/modules/request/entities/request.entity";
 import { RequestStatusEnum } from "src/modules/request/enums/status.enum";
+import { IPaginationOptions } from "src/utils/types/pagination-options";
+import { NullableType } from "src/utils/types/nullable.type";
+import { EntityCondition } from "src/utils/types/entity-condition.type";
 
 @Injectable()
 export class ReceiptService {
@@ -53,5 +56,31 @@ export class ReceiptService {
     }
 
     return request;
+  }
+
+  findManyWithPagination(paginationOptions: IPaginationOptions): Promise<ReceiptEntity[]> {
+    return this.receiptRepository.find({
+      skip: (paginationOptions.page - 1) * paginationOptions.limit,
+      take: paginationOptions.limit,
+    });
+  }
+
+  findOne(fields: EntityCondition<ReceiptEntity>): Promise<NullableType<ReceiptEntity>> {
+    return this.receiptRepository.findOne({
+      where: fields,
+    });
+  }
+
+  update(id: ReceiptEntity["id"], payload: DeepPartial<ReceiptEntity>): Promise<ReceiptEntity> {
+    return this.receiptRepository.save(
+      this.receiptRepository.create({
+        id,
+        ...payload,
+      }),
+    );
+  }
+
+  async softDelete(id: ReceiptEntity["id"]): Promise<void> {
+    await this.receiptRepository.softDelete(id);
   }
 }
