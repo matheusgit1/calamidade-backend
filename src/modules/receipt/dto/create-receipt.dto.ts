c0052f4a1cf92f233c5c660b4fe7f039d27f1046
Gustavo Valle <gustavo.vallerp@hotmail.com>
2024-05-18 22:17:10 +0200

    :sparkles: feat: adding business logic to reciept

--- src/modules/receipt/dto/create-receipt.dto.ts
+++ src/modules/receipt/dto/create-receipt.dto.ts
@@ -4,6 +4,7 @@ import { FileEntity } from "src/modules/file/entities/file.entity";
 import { IsExist } from "src/utils/validators/is-exists.validator";
 import { ReceiptTypeEnum } from "../enums/receipt-type.enum";
 import { ReceiptType } from "../entities/receipt-type.entity";
+import { RequestEntity } from "src/modules/request/entities/request.entity";
 
 export class CreateReceiptDto {
   @ApiProperty({ example: 2, type: FileEntity, description: "A chave do arquivo associado." })
@@ -13,18 +14,18 @@ export class CreateReceiptDto {
   })
   file?: FileEntity;
 
+  @ApiProperty({ example: 2, type: RequestEntity, description: "A requisicao do associado." })
+  @IsNotEmpty()
+  request: RequestEntity;
+
   @ApiProperty({ type: ReceiptType, description: "O tipo de recibo. (Nota Fiscal(0), Comprovante(1), Outro(3))", default: ReceiptTypeEnum.OTHER })
   @Validate(IsExist, ["ReceiptType", "id"], {
     message: "ReceiptNotExists",
   })
   receiptType?: ReceiptType;
 
-  @ApiProperty({ example: "2024-05-18T12:00:00Z", description: "A data e hora em que o recibo foi criado." })
-  createdAt: Date;
-
-  @ApiProperty({ example: "2024-05-18T13:30:00Z", description: "A data e hora da última atualização do recibo." })
-  updatedAt: Date;
-
-  @ApiProperty({ example: "2024-05-19T09:45:00Z", description: "A data e hora em que o recibo foi excluído." })
-  deletedAt: Date;
+  @ApiProperty({ example: 500.2, description: "O valor comprovado do recibo." })
+  @IsNotEmpty()
+  @IsNumber()
+  provenValue: number;
 }
--- src/modules/receipt/receipt.service.ts
+++ src/modules/receipt/receipt.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateReceiptDto } from "./dto/create-receipt.dto";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ReceiptEntity } from "./entities/receipt.entity";
@@ -6,16 +6,56 @@ import { DeepPartial, Repository } from "typeorm";
 import { IPaginationOptions } from "src/utils/types/pagination-options";
 import { EntityCondition } from "src/utils/types/entity-condition.type";
 import { NullableType } from "src/utils/types/nullable.type";
+import { RequestEntity } from "../request/entities/request.entity";
+import { RequestStatusEnum } from "../request/enums/status.enum";
 
 @Injectable()
 export class ReceiptService {
   constructor(
     @InjectRepository(ReceiptEntity)
     private receiptRepository: Repository<ReceiptEntity>,
+    private requestRepository: Repository<RequestEntity>,
   ) {}
 
   async create(dto: CreateReceiptDto) {
-    return this.receiptRepository.save(this.receiptRepository.create(dto));
+    // Verificar se a requisição existe e está deferida
+    const request = await this.requestRepository.findOne({
+      where: { id: +dto.request },
+      relations: ["receipts"],
+    });
+
+    if (!request) {
+      throw new BadRequestException("Requisição não encontrada.");
+    }
+
+    if (request.statusId !== RequestStatusEnum.accepted) {
+      throw new BadRequestException("Só é permitido adicionar recibos em solicitações deferidas.");
+    }
+
+    // Verificar se o valor comprovado não é superior ao valor da solicitação
+    if (dto.provenValue > request.amount) {
+      throw new BadRequestException("O valor comprovado não pode ser superior ao valor da solicitação.");
+    }
+
+    // Verificar se a soma dos valores comprovados não excede o valor da solicitação
+    const existingReceipts = await this.receiptRepository.find({ where: { request: { id: +dto.request } } });
+    const totalProvenValue = existingReceipts.reduce((sum, receipt) => sum + receipt.provenValue, 0);
+
+    if (totalProvenValue + dto.provenValue > request.amount) {
+      throw new BadRequestException("A soma dos valores comprovados de todos os recibos não pode exceder o valor total da solicitação.");
+    }
+
+    // Criar e salvar o novo recibo
+    const receipt = this.receiptRepository.create(dto);
+    const savedReceipt = await this.receiptRepository.save(receipt);
+
+    // Verificar se a solicitação deve ser alterada para deferido_liquidado
+    if (totalProvenValue + dto.provenValue === request.amount) {
+      request.statusId = RequestStatusEnum.concluded;
+      await this.requestRepository.save(request);
+    }
+
+    return savedReceipt;
   }
 
   findManyWithPagination(paginationOptions: IPaginationOptions): Promise<ReceiptEntity[]> {
