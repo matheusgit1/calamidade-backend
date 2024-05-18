8c8afc55d749c238e3f2cae9928f0d1ea0530c98
Bruno Camboim <bruno.camboim1997@gmail.com>
2024-05-18 01:58:49 -0300

    Changes validador

--- src/utils/validators/is-exists.validator.ts
+++ src/utils/validators/is-exists.validator.ts
@@ -18,7 +18,6 @@ export class IsExist implements ValidatorConstraintInterface {
   async validate(value: string, validationArguments: ValidationArguments) {
     const repository = validationArguments.constraints[0];
     const pathToProperty = validationArguments.constraints[1];
-
     const entity: unknown = await this.dataSource
       .getRepository(repository)
       .findOne({
