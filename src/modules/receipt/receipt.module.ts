3bf7de2f5bd65e13531ab91f76f9288f2454b23c
Gustavo Valle <gustavo.vallerp@hotmail.com>
2024-05-18 18:56:29 +0200

    :bug: fix: adding receipt module imports

--- src/modules/receipt/receipt.module.ts
+++ src/modules/receipt/receipt.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { ReceiptService } from './receipt.service';
-import { ReceiptController } from './receipt.controller';
+import { Module } from "@nestjs/common";
+import { ReceiptService } from "./receipt.service";
+import { ReceiptController } from "./receipt.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { IsExist } from "src/utils/validators/is-exists.validator";
+import { IsNotExist } from "src/utils/validators/is-not-exists.validator";
+import { ReceiptEntity } from "./entities/receipt.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReceiptEntity])],
   controllers: [ReceiptController],
-  providers: [ReceiptService],
+  providers: [IsExist, IsNotExist, ReceiptService],
 })
 export class ReceiptModule {}
