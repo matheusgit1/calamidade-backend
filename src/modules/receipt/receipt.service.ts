378bde3aeace64574cffa7054c798045a01b76cf
Gustavo Valle <gustavo.vallerp@hotmail.com>
2024-05-18 23:18:51 +0200

    chore: removing useless query filter

--- src/modules/receipt/receipt.service.ts
+++ src/modules/receipt/receipt.service.ts
@@ -28,7 +28,6 @@ export class ReceiptService {
   private async validateAndUpdateRequest(dto: CreateReceiptDto): Promise<RequestEntity> {
     const request = await this.requestRepository.findOne({
       where: { id: +dto.request },
-      relations: ["receipt"],
     });
 
     if (!request) {
