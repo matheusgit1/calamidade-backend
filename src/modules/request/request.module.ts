a4dedfca0fa08bf0e703fc5f208220acf6760c7a
unknown <jefersonunik@gmail.com>
2024-05-19 01:19:08 -0300

    criando o endpoint /requests/godfather

--- src/modules/request/request.module.ts
+++ src/modules/request/request.module.ts
@@ -1,12 +1,14 @@
+import { CooperatedEntity } from '../cooperated/entities/cooperated.entity';
 import { Module } from '@nestjs/common';
-import { RequestService } from './request.service';
 import { RequestController } from './request.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { RequestEntity } from './entities/request.entity';
+import { RequestService } from './request.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
+import { UsersModule } from '../user/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RequestEntity, User])],
+  imports: [TypeOrmModule.forFeature([RequestEntity, User, CooperatedEntity]), UsersModule],
   controllers: [RequestController],
   providers: [RequestService],
 })
